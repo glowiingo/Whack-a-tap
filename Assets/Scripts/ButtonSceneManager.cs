770db68515d716fbe6e5bfb31c70679d19fff68b
Alexis C. Mendiola <alexis_cm@live.com>
2020-03-17 19:37:17 -0700

    Linked StartScene to TutorialScene on button press.

---
+++ Assets/Scripts/ButtonSceneManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonSceneManager : MonoBehaviour
+{
+    [SerializeField] private string tutorialScene;
+
+    public void ButtonChangeScene()
+    {
+        SceneManager.LoadScene(tutorialScene);
+    }
+}
