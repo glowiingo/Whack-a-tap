8c2fc23b16dd812dd7b4e4693efa2bddf566fd02
Gloria Ngo <gloria.nhuto.ngo@gmail.com>
2020-04-08 21:57:08 -0700

    add cooldown for red color

--- Assets/Scripts/ColorChangeOnClick.cs
+++ Assets/Scripts/ColorChangeOnClick.cs
@@ -9,9 +9,28 @@ public class ColorChangeOnClick : MonoBehaviour
         {
             gameObject.GetComponent<Renderer>().material.color = Color.yellow;
         }
-        else { 
-            gameObject.GetComponent<Renderer>().material.color = Color.red;
+        else {
+            changeColor(gameObject);
         }
     }
-        
+
+    public void changeColor(GameObject gObject)
+    {
+        StartCoroutine(colorChangeRedYellowCoroutine(gObject));
+    }
+
+    IEnumerator colorChangeRedYellowCoroutine(GameObject gameObject)
+    {
+        //Print the time of when the function is first called.
+        // Debug.Log("Started Coroutine at timestamp : " + Time.time);
+
+        gameObject.GetComponent<Renderer>().material.color = Color.red;
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(1.0f);
+
+        gameObject.GetComponent<Renderer>().material.color = Color.yellow;
+        //After we have waited 5 seconds print the time again.
+        // Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+    }
+
 }
