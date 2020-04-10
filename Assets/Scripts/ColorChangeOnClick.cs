45d29b79b8392c0744f65213d7d71e91cdd266e3
Gloria Ngo <gloria.nhuto.ngo@gmail.com>
2020-04-10 16:43:13 -0700

    added comments, changed base numbers to constants

--- Assets/Scripts/ColorChangeOnClick.cs
+++ Assets/Scripts/ColorChangeOnClick.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class ColorChangeOnClick : MonoBehaviour
 {
+    /// <summary>
+    /// This function will update the color of the gameObject the script is attached to 
+    /// depending on whether the object has been changed to a green state or not.
+    /// </summary>
     public void updateColorOnClick() {
         if (gameObject.GetComponent<Renderer>().material.color == Color.green)
         {
@@ -14,20 +18,30 @@ public class ColorChangeOnClick : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// This function will call the coroutine to change the color
+    /// of the specified game object to red and back to yellow.
+    /// </summary>
+    /// <param name="gObject">The specified game object to change the color of.</param>
     public void changeColor(GameObject gObject)
     {
         StartCoroutine(colorChangeRedYellowCoroutine(gObject));
     }
 
+
+    /// <summary>
+    /// The coroutine changes the color of the gameObject to red and then to yellow after 1 second.
+    /// </summary>
+    /// <param name="gameObject"> The specified game obejct to change the color of. </param>
+    /// <returns></returns>
     IEnumerator colorChangeRedYellowCoroutine(GameObject gameObject)
     {
-        //Print the time of when the function is first called.
+        // For Debuging: Print the time of when the function is first called.
         // Debug.Log("Started Coroutine at timestamp : " + Time.time);
 
         gameObject.GetComponent<Renderer>().material.color = Color.red;
         //yield on a new YieldInstruction that waits for 1 seconds.
         yield return new WaitForSeconds(1.0f);
-
         gameObject.GetComponent<Renderer>().material.color = Color.yellow;
         // Debug.Log("Finished Coroutine at timestamp : " + Time.time);
     }
--- Assets/Scripts/CubeSelect.cs
+++ Assets/Scripts/CubeSelect.cs
@@ -27,6 +27,12 @@ public class CubeSelect : MonoBehaviour
     private Song song;
     private ButtonSceneManager manager;
     // Start is called before the first frame update
+
+    /// <summary>
+    /// Before the first frame update, we create multiple variables and set their values
+    /// including the specified cubes, a list of rhythm data to parse through, creating a time and count,
+    /// getting the song from the file, and creating the audio source as well as creating a button manager
+    /// </summary>
     void Start() {
         setCubes();
         // startTime = Time.time;
@@ -44,7 +50,8 @@ public class CubeSelect : MonoBehaviour
     /// <summary>
     /// Update is called once per frame. Updates the song's current playback time.
     /// If the song is still playing, it will continue with the game methods. When the song
-    /// is over, prepare to move to the next scene.
+    /// is over, prepare to move to the next scene and also overwrite the score if the 
+    /// current score is greater than the high score.
     /// </summary>
     void Update() {
         if (count < rhythmData.Count && time < lengthMilli) // song is still playing
@@ -85,7 +92,7 @@ public class CubeSelect : MonoBehaviour
     }
 
     /// <summary>
-    /// 
+    /// This function will see the cube variables for quick reference of the cube objects.
     /// </summary>
     void setCubes() {
         cubeLeft = GameObject.FindGameObjectWithTag("CubeLeft");
@@ -101,7 +108,9 @@ public class CubeSelect : MonoBehaviour
     }
 
     /// <summary>
-    /// 
+    /// This function selects a random integer correllating to one of the cubes, 
+    /// and changes the color of the specified cube to green for one second, before
+    /// changing it to yellow. It should not be allowed to pick the same cube twice in a row.
     /// </summary>
     public void pickRandomCube() {
         // pick a random cube number
@@ -113,16 +122,18 @@ public class CubeSelect : MonoBehaviour
         }
         // if it isn't set new old number to cube number
         oldNumber = cubeNumber;
+
+        // select a random cube and change its color
         GameObject randomCube = randomCubeSelector(cubeNumber);
-        // changeY(randomCube);
         changeColor(randomCube);
     }
 
     /// <summary>
-    /// 
+    /// This function will return the specified cube gameObject based on the 
+    /// cube number that was generated and selected.
     /// </summary>
-    /// <param name="cubeNumber"></param>
-    /// <returns></returns>
+    /// <param name="cubeNumber">The cube number that was generated between 1 to 3.</param>
+    /// <returns>A correlating cube.</returns>
     GameObject randomCubeSelector(int cubeNumber) {
         GameObject selectedCube = null;
         switch (cubeNumber) {
@@ -141,15 +152,17 @@ public class CubeSelect : MonoBehaviour
     }
 
     /// <summary>
-    /// 
+    /// Starts the coroutine to change the specified game object to green and back to yellow after one second.
     /// </summary>
-    /// <param name="gObject"></param>
+    /// <param name="gObject">The specified game object.</param>
     public void changeColor(GameObject gObject) {
         StartCoroutine(colorChangeCoroutine(gObject));
     }
 
     /// <summary>
-    /// 
+    /// The coroutine to change the color of the cube to green,
+    /// signaling to the player that they should 'tap' on the cube
+    /// to earn more points, and waits one second before changing the cube back to yellow.
     /// </summary>
     /// <param name="gameObject"></param>
     /// <returns></returns>
@@ -159,11 +172,9 @@ public class CubeSelect : MonoBehaviour
         // Debug.Log("Started Coroutine at timestamp : " + Time.time);
 
         gameObject.GetComponent<Renderer>().material.color = Color.green;
-        //yield on a new YieldInstruction that waits for 5 seconds.
+        //yield on a new YieldInstruction that waits for 1 second.
         yield return new WaitForSeconds(1.0f);
-
         gameObject.GetComponent<Renderer>().material.color = Color.yellow;
-        //After we have waited 5 seconds print the time again.
         // Debug.Log("Finished Coroutine at timestamp : " + Time.time);
     }
 }
